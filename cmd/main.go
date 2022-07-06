728111d7a6f2660e01dcb888f39f15cfd107a42e
Amaymon <mambetaliev.02@mail.ru>
2022-07-06 15:32:06 +0600

    Aded: config and learn library viper

--- cmd/main.go
+++ cmd/main.go
@@ -7,9 +7,14 @@ import (
 	"github.com/Maksat-luci/REST-API-TODO-service/pkg/handler"
 	"github.com/Maksat-luci/REST-API-TODO-service/pkg/repository"
 	"github.com/Maksat-luci/REST-API-TODO-service/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	//иницилизируем обьекты слоев и проводим dependency injection
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
@@ -18,8 +23,14 @@ func main() {
 	// создаём наш сервер с методом Run который конфигурирует наш сервер и начинает слушать URLы которык мы передали вторым аргументом
 	srv := new(todo.Server)
 	// начинаем слушать url адреса
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
